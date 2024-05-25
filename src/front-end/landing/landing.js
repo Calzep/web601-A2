0523b1d12a39e79dc67813c11903555e7a17ec94
Calzep <caleb-easpn@nmit.live.ac.nz>
2024-05-25 15:23:20 +1200

    Note display with dummy data

---
+++ src/front-end/landing/landing.js
@@ -0,0 +1,61 @@
+//SECTION functions
+
+const retrieveNotes = () => {
+    return noteTestData
+}
+
+const displayNotes = (notes) => {
+    let noteCards = []
+
+    notes.forEach(note => {
+        noteCards.push(`<div id="${note.id}" class="note">
+        <h3>${note.title}</h3>
+        <p class="dull date">${note.date.toLocaleDateString()}</p>
+        <hr>
+        <p class="default">${note.body}</p>
+        <a href=""><p>edit note</p></a>
+    </div>`)
+    });
+
+    noteContainer.innerHTML = noteCards.join('')
+}
+
+//SECTION variables and constants
+let noteTestData =  [
+    {
+        id: 1,
+        userId: 1,
+        title: "Test Note Title",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur aliquam magna feugiat venenatis pharetra. Nunc nisi elit, varius eget bibendum et, laoreet dictum nunc. Mauris bibendum, lacus sed vulputate laoreet, est nulla vulputate quam, a ultrices sem est et libero. Sed a viverra lectus. Vestibulum volutpat vestibulum felis vitae iaculis. Nulla elit mi, ultricies quis est et, aliquam elementum odio. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc venenatis sagittis dui sed posuere. Fusce vitae neque mauris.",
+        date: new Date('2024-05-25T02:41:30.546Z')
+    },
+    {
+        id: 2,
+        userId: 1,
+        title: "Remember to do something",
+        body: "",
+        date: new Date('2023-05-25T02:41:30.546Z')
+    },
+    {
+        id: 3,
+        userId: 1,
+        title: "Note note note note",
+        body: "Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note Note note note note ",
+        date: new Date('2024-05-24T02:41:30.546Z')
+    },
+    {
+        id: 4,
+        userId: 1,
+        title: "Chords",
+        body: "Ab Bb Db Eb F",
+        date: new Date('2024-05-01T02:41:30.546Z')
+    }
+]
+
+const noteContainer = document.getElementById('noteContainer')
+
+//SECTION Page load
+
+let notes = retrieveNotes()
+
+displayNotes(notes)
