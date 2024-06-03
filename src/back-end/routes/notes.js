76c643ddc02b49966dad04a44f22a468b110eac5
Calzep <caleb-easpn@nmit.live.ac.nz>
2024-06-04 11:31:47 +1200

    DB set up

--- src/back-end/routes/notes.js
+++ src/back-end/routes/notes.js
@@ -4,8 +4,21 @@ const router = express()
 
 //SECTION - HTTP method handlers
 
+//Get Notes
 router.get('/', (req, res) => {
     response.send('hi')
+})
+
+//Save Note
+router.post('/', (req, res) => {
+    const note = new Note ({
+        title: 'TestTitle',
+        content: 'notenotentoenotentoentoeneotnon'
+    })
+    note.save()
+    .then(result => {
+        res.redirect("/")
+    })
 })
 
 module.exports = router
--- src/back-end/server.js
+++ src/back-end/server.js
@@ -7,15 +7,15 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
 //Internal modules
-
+const Note = require('./models/note.js')
 
 //Variables
 const app = express()
 const port = process.env.PORT || 3000
 
 //Use packages
-app.use(bodyParser.json({limit: '50mb'}))
-app.use(bodyParser.urlencoded({extended: true, limit: '50mb'}))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 //SECTION - Routers
 const api = process.env.API_URL
@@ -24,10 +24,22 @@ const noteRoutes = require('./routes/notes.js')
 //use routers
 app.use(`${api}/notes`, noteRoutes)
 
-app.get('/', (req, res) => {
-    res.send('hi')
+//SECTION - Database connection
+const dbConfig = require('./config/database.config.js')
+
+mongoose.connect(dbConfig.uri, {
+    'useNewUrlParser': true,
+    'useUnifiedTopology': true,
+}).then (() => {
+    console.log(`Connection established with mongoDB ${dbConfig.uri}`)
+}).catch ((err) => {
+    console.error (`Could not connect to ${dbConfig.uri}.  Exiting process`)
+    console.error(err)
+    process.exit()
 })
+
 //SECTION - Host server
+
 app.listen(port, () => {
     console.log(`Live on port ${port}`)
 })
